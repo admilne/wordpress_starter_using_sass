bc8ac2746a0b4ffeb5cbd314661fc4fc877bb66d
Andrew Milne <andrew@18aproductions.co.uk>
2015-11-17 09:30:48 +0000

    Added colour picker to functions.php as well as code to prevent inserted images linking to itself by default

--- functions.php
+++ functions.php
@@ -257,6 +257,76 @@
 */
 
 
+/* Add Colours to TinyMCE Editory
+------------------------------------------------------------------------------------ */
+/*
+	function my_mce4_options( $init ) {
+		$default_colours = '
+			"000000", "Black",
+			"993300", "Burnt orange",
+			"333300", "Dark olive",
+			"003300", "Dark green",
+			"003366", "Dark azure",
+			"000080", "Navy Blue",
+			"333399", "Indigo",
+			"333333", "Very dark gray",
+			"800000", "Maroon",
+			"FF6600", "Orange",
+			"808000", "Olive",
+			"008000", "Green",
+			"008080", "Teal",
+			"0000FF", "Blue",
+			"666699", "Grayish blue",
+			"808080", "Gray",
+			"FF0000", "Red",
+			"FF9900", "Amber",
+			"99CC00", "Yellow green",
+			"339966", "Sea green",
+			"33CCCC", "Turquoise",
+			"3366FF", "Royal blue",
+			"800080", "Purple",
+			"999999", "Medium gray",
+			"FF00FF", "Magenta",
+			"FFCC00", "Gold",
+			"FFFF00", "Yellow",
+			"00FF00", "Lime",
+			"00FFFF", "Aqua",
+			"00CCFF", "Sky blue",
+			"993366", "Brown",
+			"C0C0C0", "Silver",
+			"FF99CC", "Pink",
+			"FFCC99", "Peach",
+			"FFFF99", "Light yellow",
+			"CCFFCC", "Pale green",
+			"CCFFFF", "Pale cyan",
+			"99CCFF", "Light sky blue",
+			"CC99FF", "Plum",
+			"FFFFFF", "White"
+			';
+		$custom_colours = '
+			"12ade8", "Redlynch Blue",
+			';
+		$init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']';
+		$init['textcolor_rows'] = 6; // expand colour grid to 6 rows
+		return $init;
+	}
+	add_filter('tiny_mce_before_init', 'my_mce4_options');
+*/
+
+
+/* Make sure images do no link to themselves by default when inserting in to a page
+------------------------------------------------------------------------------------ */
+
+	function wpb_imagelink_setup() {
+		$image_set = get_option( 'image_default_link_type' );
+
+		if ($image_set !== 'none') {
+			update_option('image_default_link_type', 'none');
+		}
+	}
+	add_action('admin_init', 'wpb_imagelink_setup', 10);
+
+
 /* Utility Functions
 ------------------------------------------------------------------------------------ */
 
